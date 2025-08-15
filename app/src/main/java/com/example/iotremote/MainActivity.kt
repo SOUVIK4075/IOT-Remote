f5bb4abd3b83a4af30d426c5b543c67256e72c1f
SOUVIK4075 <khanrasouvik112@gmail.com>
2025-08-15 23:17:59 +0530

    Add complete IoT Remote project with Android app source code

---
+++ app/src/main/java/com/example/iotremote/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.iotremote
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import com.example.iotremote.ui.theme.IoTRemoteTheme
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IoTRemoteTheme {
+
+            }
+        }
+    }
+}
+
